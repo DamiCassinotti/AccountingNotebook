ab7c555d9afbb21952dd19a096e7c16ddedbe963
DamiCassinotti <damian.cassinotti@despegar.com>
2020-07-26 23:41:20 -0300

    Added Read-Write locks to Account

--- src/main/java/com/agileengine/accounting_notebook/model/Account.kt
+++ src/main/java/com/agileengine/accounting_notebook/model/Account.kt
@@ -5,6 +5,7 @@ import com.agileengine.accounting_notebook.exception.AccountCantBeNegativeExcept
 import com.agileengine.accounting_notebook.extensions.isNegative
 import java.math.BigDecimal
 import java.util.*
+import java.util.concurrent.locks.ReentrantReadWriteLock
 
 class Account {
 
@@ -14,19 +15,25 @@ class Account {
     var transactions: ArrayList<Transaction> = ArrayList()
         private set
 
+    val rwLock = ReentrantReadWriteLock(true)
+
     fun credit(creditAmount: BigDecimal): Transaction {
+        rwLock.writeLock().lock()
         accountValue = accountValue.add(creditAmount)
         val transaction = Transaction(type = TransactionType.CREDIT, amount = creditAmount)
         transactions.add(transaction)
+        rwLock.writeLock().unlock()
         return transaction
     }
 
     fun debit(debitAmount: BigDecimal): Transaction {
+        rwLock.readLock().lock()
         if (accountValue.subtract(debitAmount).isNegative())
             throw AccountCantBeNegativeException("Insufficient funds. You can't debit $debitAmount from total.")
         accountValue = accountValue.subtract(debitAmount)
         val transaction = Transaction(type = TransactionType.DEBIT, amount = debitAmount)
         transactions.add(transaction)
+        rwLock.readLock().unlock()
         return transaction
     }
 }
